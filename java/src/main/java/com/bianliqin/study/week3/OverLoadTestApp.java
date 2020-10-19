ad5ad4f68c4e74c8ecb5912f767f915208f28857
bianliqin <2034605883@qq.com>
2020-10-19 18:55:09 +0800

    10.19

--- java/src/main/java/com/bianliqin/study/week3/OverLoadTestApp.java
+++ java/src/main/java/com/bianliqin/study/week3/OverLoadTestApp.java
@@ -1,10 +1,24 @@
 package com.bianliqin.study.week3;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @ClassName OverLoadTestApp
  * @Description TODO
  * @Author huachengyu
  * @Date 2020/10/19
  **/
+@Data
+@Builder
+@AllArgsConstructor
 public class OverLoadTestApp {
+    public static void main(String[] args) {
+        int sum1=OverLoadTest.sum(4,5);
+        System.out.println(sum1);
+        int second=OverLoadTest.second(30,80);
+        System.out.println(second);
+    }
 }
