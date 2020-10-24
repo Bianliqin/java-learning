be596ae30e530f5be3427c62af1c8371cc94cb05
bianliqin <2034605883@qq.com>
2020-10-24 18:40:33 +0800

    day05

--- java/src/main/java/com/bianliqin/study/javadaily/Circle.java
+++ java/src/main/java/com/bianliqin/study/javadaily/Circle.java
@@ -2,9 +2,20 @@ package com.bianliqin.study.javadaily;
 
 /**
  * @ClassName Circle
- * @Description TODO
+ * @Description Circle类继承Graph
  * @Author huachengyu
  * @Date 2020/10/24
  **/
-public class Circle {
+public class Circle extends BaseGraph {
+    private final double r;
+
+    public Circle(double r){
+        super();
+        this.r = r;
+    }
+
+    @Override
+    public double getCircumference(){
+    return 2 * r * Math.PI;
+    }
 }
