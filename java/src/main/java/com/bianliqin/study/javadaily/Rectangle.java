e89735f28073b85eb73427729a62b4a80e97fb1e
bianliqin <2034605883@qq.com>
2020-10-24 18:42:04 +0800

    day05

--- java/src/main/java/com/bianliqin/study/javadaily/Rectangle.java
+++ java/src/main/java/com/bianliqin/study/javadaily/Rectangle.java
@@ -2,9 +2,22 @@ package com.bianliqin.study.javadaily;
 
 /**
  * @ClassName Rectangle
- * @Description TODO
+ * @Description :Rectangle继承Graph
  * @Author huachengyu
  * @Date 2020/10/24
  **/
-public class Rectangle {
+public class Rectangle extends BaseGraph {
+    private final double length;
+    private final double width;
+
+    public Rectangle(double length,double width){
+        super();
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double getCircumference(){
+        return 2 * (length * width);
+    }
 }
