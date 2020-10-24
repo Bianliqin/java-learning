a55d42565b6db98e7b3e7f7d3b7539f99b838bbb
bianliqin <2034605883@qq.com>
2020-10-24 18:45:01 +0800

    day05

--- java/src/main/java/com/bianliqin/study/javadaily/Graph.java
+++ java/src/main/java/com/bianliqin/study/javadaily/Graph.java
@@ -6,5 +6,15 @@ package com.bianliqin.study.javadaily;
  * @Author huachengyu
  * @Date 2020/10/24
  **/
-public class Graph {
+    abstract class Graph {
+    /**
+     * 求圆形的周长
+     * @return 圆形的面积
+     */
+    public abstract double getCircumference();
+    /**
+     * 求圆形的面积
+     * @return 圆形的面积
+     */
+    public abstract double getAera();
 }
