79c87dfea8e05acb969eeeaec0712a63d2df20e6
bianliqin <2034605883@qq.com>
2020-10-26 16:58:49 +0800

    day07

--- java/src/main/java/com/bianliqin/study/week4/BaseEmployee.java
+++ java/src/main/java/com/bianliqin/study/week4/BaseEmployee.java
@@ -2,9 +2,19 @@ package com.bianliqin.study.week4;
 
 /**
  * @ClassName BaseEmployee
- * @Description TODO
+ * @Description ：基础的员工抽象类
  * @Author huachengyu
  * @Date 2020/10/26
  **/
-public class BaseEmployee {
+public  abstract class BaseEmployee {
+    public void print(){
+        System.out.println("This is an abstract class.");
+    }
+
+    /**
+     * 发工资
+     *@param
+     * @return int
+     */
+    public abstract int getSalary();
 }
