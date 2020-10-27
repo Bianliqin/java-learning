41a4bfe10e38119a354b5f90e315304904885ace
bianliqin <2034605883@qq.com>
2020-10-27 20:27:01 +0800

    day08

---
+++ java/src/main/java/com/bianliqin/study/javadaily/Test3.java
@@ -0,0 +1,15 @@
+package com.bianliqin.study.javadaily;
+
+/**
+ * @ClassName Test3
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/27
+ **/
+public class Test3 {
+    public static void main(String[] args) {
+        //控制台输出所有布尔类型常量值
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
