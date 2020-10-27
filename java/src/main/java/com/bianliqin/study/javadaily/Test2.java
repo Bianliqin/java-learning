c49d3abb7bb82fd2ff7aa8033b38e99ca280e1b6
bianliqin <2034605883@qq.com>
2020-10-27 20:23:38 +0800

    day08

---
+++ java/src/main/java/com/bianliqin/study/javadaily/Test2.java
@@ -0,0 +1,25 @@
+package com.bianliqin.study.javadaily;
+
+/**
+ * @ClassName Test2
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/10/27
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+        //控制台输出任意5行整数类型常量值
+        System.out.println(-2147483648);
+        System.out.println(-100);
+        System.out.println(0);
+        System.out.println(100);
+        System.out.println(2147483647);
+
+        //控制台输出任意5行小数类型常量值
+        System.out.println(-100.0);
+        System.out.println(-10.0);
+        System.out.println(0.0);
+        System.out.println(10.9);
+        System.out.println(100.9);
+    }
+}
