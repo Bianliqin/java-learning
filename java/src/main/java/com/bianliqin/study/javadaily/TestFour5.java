498be30e8e7c9a377d77dab85d38301a6578897e
bianliqin <2034605883@qq.com>
2020-10-31 20:45:55 +0800

    day12

--- java/src/main/java/com/bianliqin/study/javadaily/TestFour5.java
+++ java/src/main/java/com/bianliqin/study/javadaily/TestFour5.java
@@ -7,4 +7,15 @@ package com.bianliqin.study.javadaily;
  * @Date 2020/10/30
  **/
 public class TestFour5 {
+    public static void main(String[] args){
+        double random = Math.random()*100;
+        System.out.println("随机数为:");
+        System.out.println(random);
+        String str = random+"";
+        int index = str.indexOf(".");
+        //System.out.println(index);
+        String substring = str.substring(0,index + 3);
+        System.out.println("转换为:");
+        System.out.println(substring);
+    }
 }
