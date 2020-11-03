7087d04643c778ed3c2b34611cd2c2a2fd682436
bianliqin <2034605883@qq.com>
2020-11-03 22:24:45 +0800

    day15

--- java/src/main/java/com/bianliqin/study/javadaily/AnonymityOuter.java
+++ java/src/main/java/com/bianliqin/study/javadaily/AnonymityOuter.java
@@ -6,5 +6,18 @@ package com.bianliqin.study.javadaily;
  * @Author huachengyu
  * @Date 2020/11/3
  **/
-public class AnonymityOuter {
-}
+public interface AnonymityOuter {
+        void hi();
+    }
+    class AnonymityTest{
+        public static void main(String[] args) {
+            AnonymityOuter anonymityOuter = new AnonymityOuter() {
+                @Override
+                public void hi() {
+                    System.out.println("Hi,AnonymityOuter.");
+                }
+            };
+            anonymityOuter.hi();
+        }
+    }
+    
