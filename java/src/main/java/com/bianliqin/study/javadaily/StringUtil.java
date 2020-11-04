6c9788f6547722cd5ba7efcc465663b3705f2bcc
bianliqin <2034605883@qq.com>
2020-11-04 22:38:40 +0800

    day16

--- java/src/main/java/com/bianliqin/study/javadaily/StringUtil.java
+++ java/src/main/java/com/bianliqin/study/javadaily/StringUtil.java
@@ -171,3 +171,19 @@ public class StringUtil {
         return count;
     }
 }
+ class StringUtilTest {
+    public static void main(String[] args) {
+        String s = "qwda3e12qwdsa3ee";
+        String a = "a";
+        String email = "qnenbu@qq.com";
+        System.out.println("字符串s为：" + s);
+        System.out.println("-------------");
+        System.out.println("判断字符是否为空：" +  StringUtil.isEmpty(s));
+        System.out.println("-------------");
+        System.out.println("隐藏邮件地址前缀:" + StringUtil.getHideEmailPrefix(email));
+        System.out.println("-------------");
+        System.out.println("截取字符串s左侧指定长度为4的字符串:" + StringUtil.left(s, 4));
+        System.out.println("-------------");
+        System.out.println("获取子符串a在s中出现的次数:" + StringUtil.countSubStr(s,a));
+    }
+}
