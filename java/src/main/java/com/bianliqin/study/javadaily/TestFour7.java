27cd9524e5d0898b9e5fc7ebe74b071d6a0612c5
bianliqin <2034605883@qq.com>
2020-11-07 19:56:39 +0800

    day18

--- java/src/main/java/com/bianliqin/study/javadaily/TestFour7.java
+++ java/src/main/java/com/bianliqin/study/javadaily/TestFour7.java
@@ -1,5 +1,7 @@
 package com.bianliqin.study.javadaily;
 
+import java.util.Scanner;
+
 /**
  * @ClassName TestFour7
  * @Description TODO
@@ -7,4 +9,22 @@ package com.bianliqin.study.javadaily;
  * @Date 2020/11/7
  **/
 public class TestFour7 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+        boolean p = isP(next);
+        System.out.println("回文数:" + p);
+    }
+    public static boolean isP(String str){
+        int start = 0;
+        int end = str.length()-1;
+        while (start<end){
+            if(str.charAt(start) != str.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
 }
