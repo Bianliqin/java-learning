0cfedd4a39eb014421ffdaf44dacd7327fd94b55
bianliqin <2034605883@qq.com>
2020-11-08 21:33:39 +0800

    day20

---
+++ java/src/main/java/com/bianliqin/study/javadaily/SwingLoginExample.java
@@ -0,0 +1,10 @@
+package com.bianliqin.study.javadaily;
+
+/**
+ * @ClassName SwingLoginExample
+ * @Description TODO
+ * @Author huachengyu
+ * @Date 2020/11/8
+ **/
+public class SwingLoginExample {
+}
