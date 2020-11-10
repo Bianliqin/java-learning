8f7568ced9a84935f9548607832df7f775a8e42f
bianliqin <2034605883@qq.com>
2020-11-10 21:04:42 +0800

    day21

--- java/src/main/java/com/bianliqin/study/javadaily/TestFour8.java
+++ java/src/main/java/com/bianliqin/study/javadaily/TestFour8.java
@@ -1,5 +1,7 @@
 package com.bianliqin.study.javadaily;
 
+import java.util.Scanner;
+
 /**
  * @ClassName TestFour8
  * @Description TODO
@@ -7,4 +9,31 @@ package com.bianliqin.study.javadaily;
  * @Date 2020/11/7
  **/
 public class TestFour8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        String next = scanner.next();
+        int b = scanner.nextInt();
+        String count = count(a, next, b);
+        System.out.println(a + next + b + "#" + count);
+    }
+
+    public static String count(int a, String op, int b) {
+        int r = 0;
+        if ("+".equals(op)) {
+            r = a + b;
+        } else if ("-".equals(op)) {
+            r = a - b;
+        } else if ("*".equals(op)) {
+            r = a * b;
+        } else if ("/".equals(op)) {
+            r = a / b;
+        } else if ("%".equals(op)) {
+            r = a % b;
+        } else {
+            return null;
+        }
+
+        return r + "";
+    }
 }
