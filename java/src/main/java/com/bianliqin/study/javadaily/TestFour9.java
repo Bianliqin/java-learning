7bf9a0914d7b20f3032438d3349f94330654b646
bianliqin <2034605883@qq.com>
2020-11-10 21:05:44 +0800

    day21

--- java/src/main/java/com/bianliqin/study/javadaily/TestFour9.java
+++ java/src/main/java/com/bianliqin/study/javadaily/TestFour9.java
@@ -7,4 +7,33 @@ package com.bianliqin.study.javadaily;
  * @Date 2020/11/7
  **/
 public class TestFour9 {
+    public static void main(String[] args) {
+        String s = "qweRY123";
+        System.out.println(s + "密码是否合法:" + isV(s));
+    }
+
+    public static boolean isV(String pwd) {
+        if (pwd.length() < 8) {
+            return false;
+        }
+
+        int countA = 0;
+        char[] chars = pwd.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char ch = chars[i];
+            //2个大写字符
+            if (ch >= 'A' && ch <= 'Z') {
+                countA++;
+            }
+
+            //字母数字
+            if ((ch < '0' || ch > '9') && (ch < 'A' || ch > 'Z') && (ch < 'a' || ch > 'Z')) {
+                return false;
+            }
+        }
+        if (countA < 2) {
+            return false;
+        }
+        return true;
+    }
 }
