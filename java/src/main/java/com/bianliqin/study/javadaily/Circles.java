9c4d7adc0e0cee1ce9f30c803b19533b8304645b
bianliqin <2034605883@qq.com>
2020-11-12 18:47:10 +0800

    day23

--- java/src/main/java/com/bianliqin/study/javadaily/Circles.java
+++ java/src/main/java/com/bianliqin/study/javadaily/Circles.java
@@ -7,4 +7,22 @@ package com.bianliqin.study.javadaily;
  * @Date 2020/11/12
  **/
 public class Circles {
+    int r;
+    public Circles(){
+    }
+    public Circles(int r){
+        this.r = r;
+    }
+    public int getR(){
+        return r;
+    }
+    public void setR(int r){
+        this.r = r;
+    }
+    public void showArea(){
+        System.out.println("半径为:"+r+",面积为:"+(3.14*r*r));
+    }
+    public void showPerimeter(){
+        System.out.println("半径为："+r+",面积为:"+(2*3.14*r));
+    }
 }
