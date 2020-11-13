03ec6c779ef18f6a605530ddd63cb47ea09164b4
bianliqin <2034605883@qq.com>
2020-11-13 19:06:22 +0800

    day24

--- java/src/main/java/com/bianliqin/study/javadaily/Card.java
+++ java/src/main/java/com/bianliqin/study/javadaily/Card.java
@@ -7,4 +7,15 @@ package com.bianliqin.study.javadaily;
  * @Date 2020/11/13
  **/
 public class Card {
+    //点数
+    private String ds;
+    //花色
+    private String hs;
+    public Card(String ds,String hs){
+        this.ds = ds;
+        this.hs = hs;
+    }
+    public void showCard(){
+        System.out.println(ds + hs);
+    }
 }
