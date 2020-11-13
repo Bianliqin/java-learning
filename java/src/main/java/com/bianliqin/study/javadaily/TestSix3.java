f40b071055173389a4a2e4671e4a9154ed53a075
bianliqin <2034605883@qq.com>
2020-11-13 19:07:18 +0800

    day24

--- java/src/main/java/com/bianliqin/study/javadaily/TestSix3.java
+++ java/src/main/java/com/bianliqin/study/javadaily/TestSix3.java
@@ -7,4 +7,8 @@ package com.bianliqin.study.javadaily;
  * @Date 2020/11/13
  **/
 public class TestSix3 {
+    public static void main(String[] args){
+        Card card = new Card("黑桃","A");
+        card.showCard();
+    }
 }
