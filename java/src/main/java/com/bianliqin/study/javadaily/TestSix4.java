c7925df88381a715e8715823e961f362145df4be
bianliqin <2034605883@qq.com>
2020-11-14 18:28:04 +0800

    day25

--- java/src/main/java/com/bianliqin/study/javadaily/TestSix4.java
+++ java/src/main/java/com/bianliqin/study/javadaily/TestSix4.java
@@ -1,5 +1,7 @@
 package com.bianliqin.study.javadaily;
 
+import java.awt.*;
+
 /**
  * @ClassName TestSix4
  * @Description TODO
@@ -7,4 +9,19 @@ package com.bianliqin.study.javadaily;
  * @Date 2020/11/14
  **/
 public class TestSix4 {
+    public static void main(String[] args){
+        int[] salary = {15000,3000};
+        Manager m = new Manager("James","9527",salary);
+        m.intro();
+        m.showSalary();
+        m.work();
+        System.out.println("==============");
+        Coder c = new Coder();
+        c.setName("Kobe");
+        c.setId("0025");
+        c.setSalary(10000);
+        c.intro();
+        c.showSalary();
+        c.work();
+    }
 }
