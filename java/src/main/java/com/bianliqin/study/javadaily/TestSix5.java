2f885df2e5238dd978e40b8ff3398ffdb61921e6
bianliqin <2034605883@qq.com>
2020-11-15 22:46:18 +0800

    day26

--- java/src/main/java/com/bianliqin/study/javadaily/TestSix5.java
+++ java/src/main/java/com/bianliqin/study/javadaily/TestSix5.java
@@ -7,4 +7,15 @@ package com.bianliqin.study.javadaily;
  * @Date 2020/11/15
  **/
 public class TestSix5 {
+    public static void main(String[] args){
+        Teacher t = new Teacher();
+        t.setName("周老师");
+        t.setAge(30);
+        t.setContent("java面向对象");
+        t.eat();
+        t.jiangke();
+        Stu stu = new Stu("韩同学",18,"java面向对象");
+        stu.eat();
+        stu.study();
+    }
 }
