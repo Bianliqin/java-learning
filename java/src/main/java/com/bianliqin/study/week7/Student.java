e3ffde0ddea64b86ddd889cd31cb78d14887eaf5
bianliqin <2034605883@qq.com>
2020-11-16 19:31:25 +0800

    day27

--- java/src/main/java/com/bianliqin/study/week7/Student.java
+++ java/src/main/java/com/bianliqin/study/week7/Student.java
@@ -1,10 +1,33 @@
 package com.bianliqin.study.week7;
 
+import com.bianliqin.study.javadaily.Stu;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @ClassName Student
  * @Description TODO
  * @Author huachengyu
  * @Date 2020/11/16
  **/
-public class Student {
+@Builder
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class Student implements Comparable<Student> {
+    private String name;
+    private Integer age;
+
+    @Override
+    public int compareTo(Student o) {
+        if (this.getAge() > o.getAge()) {
+            return 1;
+        } else if (this.getAge() < o.getAge()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
