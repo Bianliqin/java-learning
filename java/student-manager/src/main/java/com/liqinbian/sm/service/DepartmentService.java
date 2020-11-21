57c758cf50a20ef9f6db899e99f7777f86feba53
bianliqin <2034605883@qq.com>
2020-11-21 21:50:36 +0800

    day31

--- java/student-manager/src/main/java/com/liqinbian/sm/service/DepartmentService.java
+++ java/student-manager/src/main/java/com/liqinbian/sm/service/DepartmentService.java
@@ -1,5 +1,9 @@
 package com.liqinbian.sm.service;
 
+import com.liqinbian.sm.entity.Department;
+
+import java.util.List;
+
 /**
  * @ClassName DepartmentService
  * @Description TODO
@@ -7,4 +11,18 @@ package com.liqinbian.sm.service;
  * @Date 2020/11/18
  **/
 public interface DepartmentService {
+    /**
+     * 查询所有院系
+     *
+     * @return List<Department>
+     */
+    List<Department> selectAll();
+    /**
+     * 新增院系
+     *
+     * @param department 入参
+     * @return int
+     */
+    int addDepartment(Department department);
+    int delDepartment(Department department,Integer id);
 }
